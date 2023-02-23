49e8d89aaf66e160a9f40d8f5b092fa5464a2e85
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-02-23 00:05:28 -0300

    Banco ok

--- src/main/java/teste.java
+++ src/main/java/teste.java
@@ -1,6 +1,10 @@
 
+import br.com.exception.DBException;
 import br.com.infra.DB;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -15,8 +19,23 @@ public class teste {
     
     public static void main(String[] args) {
         
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet query = null;
         
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            query = st.executeQuery("select * from public.tbestado");
+            
+            while(query.next()){
+                System.out.println(query.getInt(1) + " - " + query.getString(2) + " - " + query.getString(3));
+            }
+        } catch (SQLException e){
+            throw new DBException(e.getMessage());
+        }
+        
+        // Banco rodando.
         
     }
     
